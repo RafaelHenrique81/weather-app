60a9015799493b41988a4ac1107455483f03f5bc
Rafael Henrique <rhsuptec@gmail.com>
2022-08-24 19:13:45 -0300

    created response whith moked city

--- js/weather.js
+++ js/weather.js
@@ -1,2 +1,22 @@
 const APIKey = '6znLkOt4xm2qHTUFeSGp5W7kiyNUXoYt'
 
+const getCityUrl = cityName => 
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+    try {
+        const cityUrl = getCityUrl(cityName)
+        const response = await fetch(cityUrl)
+
+        if(!response.ok){
+            throw new Error('Não foi possível obter os dados')
+        }
+
+        const [cityData] = await response.json()
+        return cityData
+    } catch ({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityData('São Paulo')
