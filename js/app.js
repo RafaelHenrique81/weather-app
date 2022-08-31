5717491a88b143b8acc360be4decd621dacf2c87
Rafael Henrique <rhsuptec@gmail.com>
2022-08-31 10:43:40 -0300

    added dynamically fill icons and images

--- js/app.js
+++ js/app.js
@@ -3,15 +3,30 @@ const cityWeather = document.querySelector('[data-js="weather-details"]')
 const cityName = document.querySelector('[data-js="city-name"]') 
 const cityWeatherDetail = document.querySelector('[data-js="city-weather"]') 
 const cityTemperature = document.querySelector('[data-js="city-temperature"]') 
+const cityCard = document.querySelector('[data-js="city-card"]')
+const timeIcon = document.querySelector('[data-js="time-icon"]')
 
+let timeImg = document.querySelector('[data-js="time"]')
 
 cityForm.addEventListener('submit', async event =>{
     event.preventDefault()
 
     const inputValue = event.target.city.value
     const [{ Key, LocalizedName }] = await getCityData(inputValue)
-    const [{ WeatherText, Temperature}] = await getCityWeather(Key)
+    const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+    const timeIconImg = `<img src="./src/icons/${WeatherIcon}.svg" />`
 
+    if (cityCard.classList.contains('d-none')) {
+        cityCard.classList.remove('d-none')
+    }
+
+    if (IsDayTime){
+        timeImg.src = './src/day.svg'
+    } else {
+        timeImg.src = './src/night.svg'
+    }
+
+    timeIcon.innerHTML = timeIconImg
     cityName.textContent = LocalizedName
     cityWeatherDetail.textContent = WeatherText
     cityTemperature.textContent = Temperature.Metric.Value
